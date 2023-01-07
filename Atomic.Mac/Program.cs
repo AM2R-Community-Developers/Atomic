9526ab0c450c68081168c5ea7844eb6c6a408ea4
Miepee <janbidler00@protonmail.com>
2023-01-07 13:50:43 +0100

    Show message if application crashes

--- Atomic.Mac/Program.cs
+++ Atomic.Mac/Program.cs
@@ -7,6 +7,23 @@ class Program
     [STAThread]
     static void Main(string[] args)
     {
-        new Application(Eto.Platforms.Mac64).Run(new ModPacker());
+        var application = new Application(Eto.Platforms.Mac64);
+        application.UnhandledException += ApplicationOnUnhandledException;
+        try
+        {
+            application.Run(new ModPacker());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Unhandled Exception!\n*****Stack Trace*****\n\n{e}");
+        }
+        
+    }
+    private static void ApplicationOnUnhandledException(object sender, Eto.UnhandledExceptionEventArgs e)
+    {
+        Application.Instance.Invoke(() =>
+        {
+            MessageBox.Show($"Unhandled Exception!\n*****Stack Trace*****\n\n{e.ExceptionObject}", "GTK", MessageBoxType.Error);
+        });
     }
 }
