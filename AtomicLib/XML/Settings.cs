1b294be68164fbb66dc9c558e62046983bd93b69
LiveLM <livelm@hotmail.com>
2023-03-03 13:32:55 -0300

    Add settings class

---
+++ AtomicLib/XML/Settings.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Serialization;
+
+namespace AtomicLib.XML;
+
+[Serializable]
+[XmlRoot("message")]
+public class Settings
+{
+    [XmlAttribute("Language")]
+    public string Language
+    { get; set; }
+    
+    [XmlAttribute("FillInContents")]
+    public bool FillInContents
+    { get; set; }
+
+    public Settings()
+    // default settings
+    {
+        Language = "English";
+        FillInContents = false;
+    }
+}
