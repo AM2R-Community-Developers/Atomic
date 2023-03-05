b96fa598e39d5ec2946149ab6546bb74c2917c27
LiveLM <livelm@hotmail.com>
2023-03-05 05:48:54 -0300

    Create functions for loading, saving and creating the default config

--- AtomicLib/XML/Config.cs
+++ AtomicLib/XML/Config.cs
@@ -6,6 +6,8 @@ namespace AtomicLib.XML;
 [XmlRoot("message")]
 public class Config
 {
+    public static string ConfigFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Atomic/config.xml");
+    
     [XmlAttribute("Language")]
     public string Language
     { get; set; }
@@ -19,5 +21,35 @@ public class Config
     {
         Language = "English";
         FillInContents = false;
+    }
+    
+    // functions
+    public static Config LoadAndReturnConfig()
+    {
+        Config config = null;
+        if (File.Exists(ConfigFilePath))
+        {
+            string configXml = File.ReadAllText(ConfigFilePath);
+            config = Serializer.Deserialize<Config>(configXml);
+        }
+        else
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(ConfigFilePath));
+            config = CreateDefaultConfig();
+        }
+        return config;
+    }
+    
+    public static Config CreateDefaultConfig()
+    {
+        Config defaultConfig = new Config();
+        SaveConfig(defaultConfig);
+        return defaultConfig;
+    }
+    
+    public static void SaveConfig(Config config)
+    {
+        string xmlOutput = Serializer.Serialize<Config>(config);
+        File.WriteAllText(ConfigFilePath, xmlOutput);
     }
 }
