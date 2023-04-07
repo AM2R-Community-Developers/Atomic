25189cb7b769a25382fe6ac0234b6b54675e409b
LiveLM <livelm@hotmail.com>
2023-04-07 14:56:05 -0300

    Add documentation to Config.cs and FieldContents.cs

--- AtomicLib/Config.cs
+++ AtomicLib/Config.cs
@@ -3,20 +3,32 @@ using AtomicLib.XML;
 
 namespace AtomicLib;
 
+/// <summary>
+/// Class that handles saving and loading the modpacker configuration.
+/// </summary>
 [Serializable]
 [XmlRoot("config")]
 public class Config
 {
     public static string ConfigFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Atomic", "config.xml");
     
+    /// <summary>
+    /// Language used for the modpacker.
+    /// </summary>
     [XmlAttribute("Language")]
     public string Language
     { get; set; }
     
+    /// <summary>
+    /// Determines if the modpacker should remember information entered into the fields between usages.
+    /// </summary>
     [XmlAttribute("FillInContents")]
     public bool FillInContents
     { get; set; }
     
+    /// <summary>
+    /// Used to save information from the fields. Only used when <see cref="FillInContents"/> is <see langword="true"/>
+    /// </summary>
     [XmlElement("Fields")]
     public FieldContents Fields
     { get; set; }
@@ -33,6 +45,10 @@ public class Config
         Fields = new FieldContents();
     }
     
+    /// <summary>
+    /// Reads the configuration from disk and returns a <see cref="Config"/> object. If the configuration does not exist, a default configuration will be created.
+    /// </summary>
+    /// <returns> Returns a <see cref="Config"/> object containing either the configuration read from disk or the default one.</returns>
     public static Config LoadAndReturnConfig()
     {
         if (!File.Exists(ConfigFilePath))
@@ -41,6 +57,10 @@ public class Config
         return Serializer.Deserialize<Config>(File.ReadAllText(ConfigFilePath));     
     }
     
+    /// <summary>
+    /// Saves a default configuration to disk and returns it, creating the config folders if necessary.
+    /// </summary>
+    /// <returns> Returns the <see cref="Config"/> object.</returns>
     public static Config CreateAndReturnDefaultConfig()
     {
         Directory.CreateDirectory(Path.GetDirectoryName(ConfigFilePath));
@@ -49,6 +69,10 @@ public class Config
         return defaultConfig;
     }
     
+    /// <summary>
+    /// Writes the <see cref="Config"/> to disk.
+    /// </summary>
+    /// <param name="config">The <see cref="Config"/> that should be saved to the disk.</param>
     public static void SaveConfig(Config config)
     {
         string xmlOutput = Serializer.Serialize<Config>(config);
